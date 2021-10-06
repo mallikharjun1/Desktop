e74ff8b4618698cd865c01995838851ed6aa6044
Your Name <you@example.com>
2021-10-06 15:20:00 +0530

    j

---
+++ Untitled1.ps1
@@ -0,0 +1,17 @@
+﻿Connect-AzAccount
+New-AzStorageAccount -ResourceGroupName kinley -Name manstrg -Location "Northeurope" -SkuName Standard_LRS -Kind StorageV2 -AccessTier Hot 
+Get-AzStorageAccount -ResourceGroupName kinley 
+location = "eastus"
+$resourceGroup = "bmrrg"
+New-AzResourceGroup -ResourceGroupName $resourceGroup -Location $location
+$storageAccountName = "bmrdemo"
+$storageAccount = New-AzStorageAccount -ResourceGroupName $resourceGroup `
+  -Name $storageAccountName `
+  -Location $location `
+  -SkuName Standard_LRS `
+  -Kind Storage
+
+$ctx = $storageAccount.Context
+$tableName = "pshtesttable"
+New-AzStorageTable –Name $tableName –Context $ctx
+New-Azvirt
---
+++ str2.ps1
@@ -0,0 +1,9 @@
+﻿
+$storgacnt=@('king32','queen54','god43','mass')
+
+foreach($G in $storgacnt )
+
+{
+
+New-AzStorageAccount -ResourceGroupName maggie -Name $G  -SkuName Standard_LRS -Location "west us" -Kind StorageV2 -AccessTier Cool
+}
