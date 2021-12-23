242d494200fbd494b59e895acf3cc0214f681664
Your Name <you@example.com>
2021-12-23 14:41:26 +0530

    first commit

--- str2.ps1
+++
@@ -1,9 +0,0 @@
-﻿
-$storgacnt=@('king32','queen54','god43','mass')
-
-foreach($G in $storgacnt )
-
-{
-
-New-AzStorageAccount -ResourceGroupName maggie -Name $G  -SkuName Standard_LRS -Location "west us" -Kind StorageV2 -AccessTier Cool
-}
